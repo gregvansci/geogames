a99658e6a93e3c7638929a2477b811b33c2351f6
gregvansci <gregvansci76@hotmail.com>
2022-07-13 16:04:43 -0700

    Home page map

--- pages/components/WelcomeMap.tsx
+++ pages/components/WelcomeMap.tsx
@@ -1,14 +1,33 @@
-// import {
-//   ComposableMap
-//   Geographies,
-//   Geography,
-//   Square,
-//   Graticule
-// } from "react-simple-maps";
+import React from "react";
+import ReactDOM from "react-dom"
+import { ComposableMap, Geographies, Geography, Sphere, Graticule } from "react-simple-maps";
+
+
 
 export default function WelcomeMap () {
+
+  var randomColor = Math.floor(Math.random()*16777215).toString(16);
+  console.log(randomColor);
+
   return (
-    <>
-    </>
+    <div>
+      <ComposableMap
+        projectionConfig={{
+          rotate: [-10, 0, 0],
+          scale: 147
+        }}
+      >
+        <Sphere stroke="#1F2937" strokeWidth={0.5} fill="#E4E5E6" id="" />
+        <Graticule stroke="#1F2937" strokeWidth={0.5} />
+        <Geographies geography="/features.json">
+          {({ geographies }) =>
+            geographies.map((geo) => (
+              <Geography key={geo.rsmKey} geography={geo} fill="#1F2937" />
+            ))
+          }
+        </Geographies>
+        
+      </ComposableMap>
+    </div>
   )
 }
