1b65bbaa046d04ed47a20ca4309e83201fff9d90
gregvansci <gregvansci76@hotmail.com>
2022-07-13 16:10:19 -0700

    removing console log statement

--- pages/components/WelcomeMap.tsx
+++ pages/components/WelcomeMap.tsx
@@ -6,9 +6,6 @@ import { ComposableMap, Geographies, Geography, Sphere, Graticule } from "react-
 
 export default function WelcomeMap () {
 
-  var randomColor = Math.floor(Math.random()*16777215).toString(16);
-  console.log(randomColor);
-
   return (
     <div>
       <ComposableMap
