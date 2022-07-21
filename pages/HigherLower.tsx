1ac9c84dfc6ef8dd561bcce11f8d91771bb2c8cd
gregvansci <gregvansci76@hotmail.com>
2022-07-20 18:10:24 -0700

    Higher lower game logic

--- pages/HigherLower.tsx
+++ pages/HigherLower.tsx
@@ -3,8 +3,9 @@ import Head from "next/head";
 import Navbar from "./components/Navbar";
 import WorldCountriesMap from "./maps/WorldCountriesMap";
 import React, { useState } from 'react'
-import { auth } from '../firebase/firebase-config'
+import { auth, db } from '../firebase/firebase-config'
 import { onAuthStateChanged } from 'firebase/auth'
+import { collection, getDocs } from 'firebase/firestore'
 import { useAuthState } from 'react-firebase-hooks/auth'
 import AuthModal from "./components/AuthModal";
 
@@ -24,15 +25,63 @@ const HigherLower: NextPage = () => {
     setOpenProfile(!openProfile);
   }
 
+  var leftCountry: string = "China";
+  var rightCountry: string = "United States";
+  var leftCountryValue: number = 1400000000;
+  var rightCountryValue: number = 330000000;
+
+  var category: string = "Population";
+  var highScore: number = 0;
+
+  const [gameOver, setGameOver] = useState(false);
+  const [roundOver, setRoundOver] = useState(false);
+  const [currentScore, setCurrentScore] = useState(0);
   
+  function handleChoice(choice: number) {
+    if (gameOver) { return; }
+    if (roundOver) { return; } 
 
-  var leftCountry = "China";
-  var rightCountry = "United States";
+    if (choice === 1) {
+      console.log("Left Country Chosen");
+      if (leftCountryValue > rightCountryValue) {
+        setCurrentScore(currentScore + 1);
+        if (currentScore > highScore) {
+          highScore = currentScore;
+        }
+      } else {
+        setGameOver(true);
+      }
+    } else {
+      console.log("Right Country Chosen");
+      if (leftCountryValue < rightCountryValue) {
+        setCurrentScore(currentScore + 1);
+        if (currentScore > highScore) {
+          highScore = currentScore;
+        }
+      } else {
+        setGameOver(true);
+      }
+    }
+    console.log(gameOver);
+    setRoundOver(true);
+    if(!gameOver) {
+      setTimeout(() => {
+        setRoundOver(false);
+      }, 3000);
+    }
+  }
 
-  var category = "Population";
+ 
 
-  var currentScore = 0;
-  var highScore = 0;
+
+  // const colRef = collection(db, "countries");
+  // getDocs(colRef)
+  //   .then((snapshot) => {
+  //     console.log(snapshot.docs);
+  //   })
+  //   .catch((error) => {
+  //     console.log(error);
+  //   });
 
   return (
     <div className="flex font-sans flex-col items-center h-auto max-h-screen bg-primary-dark min-w-[600px]">
@@ -50,20 +99,22 @@ const HigherLower: NextPage = () => {
           <div className="bg-tertiary-dark overflow-hidden w-[450px] lg:w-[550px] md:h-[40vh] absolute top-[45vh] rounded-xl flex flex-col">
             <div className="mt-2 text-center">
               <p className="text-base text-gray-200">Higher Lower</p>
-              <p className="text-2xl font-bold text-">{category}</p>
+              <p className={`text-2xl font-bold ${roundOver ? gameOver ? 'text-red-500' : 'text-green-500' : 'text-white'}`}>{category}</p>
             </div>
-            <div className="flex flex-row justify-between w-full px-4 pt-8 text-sm">
-              <div className="flex flex-col items-center w-24 gap-4 pt-1">
-                <p className="">{leftCountry}</p>
-                <p className="underline whitespace-pre">                        </p>
+            <div className={"flex flex-row justify-between w-full px-4 lg:px-8 pt-8 text-sm"}>
+              <div className="relative flex flex-col items-center w-24 pt-1">
+                <p className="mb-4">{leftCountry}</p>
+                <p>{roundOver ? leftCountryValue : ""}</p>
+                <p className="absolute underline whitespace-pre bottom-10">                        </p>
               </div>
               <div className="flex flex-col">
-                <button className="px-8 pb-1 mb-3 text-3xl font-medium text-center ring bg-secondary-dark ring-indigo-600 hover:bg-indigo-600 rounded-xl">&gt;</button>
-                <button className="px-8 pb-1 mb-3 text-3xl font-medium text-center ring bg-secondary-dark ring-indigo-600 hover:bg-indigo-600 rounded-xl">&lt;</button>
+                <button onClick={event => handleChoice(1)} className="px-8 pb-1 mb-3 text-3xl font-medium text-center ring bg-secondary-dark ring-indigo-600 hover:bg-indigo-600 rounded-xl">&gt;</button>
+                <button onClick={event => handleChoice(2)} className="px-8 pb-1 mb-3 text-3xl font-medium text-center ring bg-secondary-dark ring-indigo-600 hover:bg-indigo-600 rounded-xl">&lt;</button>
               </div>
-              <div className="flex flex-col items-center w-24 gap-4 pt-1">
-                <p className="">{rightCountry}</p>
-                <p className="underline whitespace-pre">                        </p>
+              <div className="relative flex flex-col items-center w-24 pt-1">
+                <p className="mb-4">{rightCountry}</p>
+                <p>{roundOver ? rightCountryValue : ""}</p>
+                <p className="absolute underline whitespace-pre bottom-10">                        </p>
               </div>
 
             </div>
